0b01b461c4dbef94c81c1c452b430aca4affdba6
Mark Edler <mark.edler@acurus.com.au>
2025-04-05 23:49:33 +1100

    auth routes

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { User } = require("../models/User");
+
+module.exports = function (req, res, next) {
+  // Get token from the header
+  const token = req.header("x-auth-token");
+
+  // Check if not token
+  if (!token) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
+};
